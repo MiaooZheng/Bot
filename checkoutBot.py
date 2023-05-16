7cb1a26ae9893b789e21db455874199ecd5d2869
Miao Zheng <zhengmiao0810@tgmail.com>
2023-05-16 14:21:12 -0400

    add color selection

--- checkoutBot.py
+++ checkoutBot.py
@@ -15,7 +15,7 @@ product_url = 'https://www.birkenstock.com/ca/boston-suede-leather/boston-suede-
 
 
 class CheckoutBot:
-    def __init__(self, path):
+    def __init__(self, path, color = 'Taupe'):
         self.options = Options()
         self.options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
         self.options.add_argument("--disable-popup-blocking")
@@ -27,6 +27,40 @@ class CheckoutBot:
         self.driver = webdriver.Chrome(service = self.service, options = self.options)
         # self.driver.switch_to.alert.dismiss()
         self.driver.maximize_window()
+        self.color = color
+
+
+    def search_product(self): # here i have specific item
+        self.driver.get("https://www.birkenstock.com/ca/boston-suede-leather/boston-suede-suedeleather-softfootbed-eva-u_46.html?dwvar_boston-suede-suedeleather-softfootbed-eva-u__46_width=N")
+        time.sleep(8)
+        
+        # remove ads first 
+        WebDriverWait(self.driver, 40).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//iframe[@id="attentive_creative"]')))
+        WebDriverWait(self.driver, 40).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='page1']//button[@id='closeIconContainer']")))
+
+        self.close_ads = self.driver.find_element(By.XPATH, "//div[@id='page1']//button[@id='closeIconContainer']")
+        self.close_ads.click()
+        
+        # then choose color of shoes 
+        # actually i want to buy this one - but i'll test the second one
+
+        colors = {'Taupe': '560771', "Mocha": '660461', "Mink": '1009543', "Black": '660473'}
+
+        for color in colors:
+            if color == self.color:
+                color_id = colors[color] 
+        print(color_id)
+
+        WebDriverWait(self.driver, 40).until(EC.element_to_be_clickable((By.XPATH, f"//img[@class='variant-image' and contains(@src, {color_id})]")))
+        self.choose_color = self.driver.find_element(By.XPATH, f"//img[@class='variant-image' and contains(@src, {color_id})]")
+        self.choose_color.click()
+        
+        # now check width of shoes
+        # //span[.='Regular/Wide']
+
+
+
+
 
     def login(self):
         from selenium.webdriver.common.action_chains import ActionChains
@@ -49,27 +83,14 @@ class CheckoutBot:
         
 
 
-
-
-
-
-    def search_product(self): # here i have specific item
-        self.driver.get("https://www.birkenstock.com/ca/boston-suede-leather/boston-suede-suedeleather-softfootbed-eva-u_46.html?dwvar_boston-suede-suedeleather-softfootbed-eva-u__46_width=N")
-        time.sleep(30)
         
-        # self.driver.switch_to.alert.dismiss()
-
-        self.close_ads = self.driver.find_element(By.XPATH, '//*[@id="closeIconSvg"]').click()
-
-        # self.search = self.driver.find_element(By.XPATH, '//*[@id="product-content"]/div[3]/div[2]/ul/li[1]/div[2]/div/div/div/div[1]/div/div/a/img').click()
-        self.search = self.driver.find_element(By.XPATH, '//*[@id="product-content"]/div[3]/div[2]/ul/li[1]/div[2]/div/div/div/div[2]/div/div/a/img')
-        self.driver.switch_to.alert.dismiss()
-        self.search.click()
-
-
-client = CheckoutBot(path = "/Users/miaoz/Desktop/github_projects/bot/chromedriver_mac64/chromedriver")
-client.login()
+####### when color is Mink, there's an error need to be fixed 
+client = CheckoutBot(path = "/Users/miaoz/Desktop/github_projects/bot/chromedriver_mac64/chromedriver", color = 'Mocha')
+client.search_product()
 
 
 
 # https://www.birkenstock.com/ca/boston-suede-leather/boston-suede-suedeleather-softfootbed-eva-u_46.html?dwvar_boston-suede-suedeleather-softfootbed-eva-u__46_width=N
+
+
+# notes: when color is Mink, there's an error. Other colors working
